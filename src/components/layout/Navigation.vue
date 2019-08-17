bcfabe530f6e242475c3ecdc5f0c713c982482db
sustained <sustained.dissonance@gmail.com>
2019-08-17 22:34:11 +0100

    Add navigation.

---
+++ src/components/layout/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item href="/" class="has-text-weight-semibold">fourier-transform</b-navbar-item>
+    </template>
+
+    <template slot="start">
+      <b-navbar-item href="#">Home</b-navbar-item>
+      <b-navbar-item href="https://www.youtube.com/watch?v=spUNpyF58BY">Video</b-navbar-item>
+      <b-navbar-item href="https://github.com/sustained/fourier-transform">Repository</b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
