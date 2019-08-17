0e034dc14f71818e55ef397435250ae045017d76
sustained <sustained.dissonance@gmail.com>
2019-08-17 22:33:35 +0100

    Switch to teoria.

--- src/library/Notes.js
+++ src/library/Notes.js
@@ -1,41 +1,17 @@
-const A4 = 440; // Hz
-const POSITION_OF_A4 = 49;
-const TWELFTH_ROOT_OF_TWO = Math.pow(2, 1 / 12);
+import teoria from "teoria";
 
-export const NOTES_PER_OCTAVE = 12;
-export const NOTES_ON_GRAND_PIANO = 88;
-
-export function keyToFrequency(key) {
-  return Math.pow(TWELFTH_ROOT_OF_TWO, key - POSITION_OF_A4) * A4;
-}
-
-export function frequencyToKey(freq) {
-  return Math.floor(NOTES_PER_OCTAVE * Math.log2(freq / A4) + POSITION_OF_A4);
-}
-
+const NOTE_NAMES = ["A", "A#", "B", "C", "Db", "D", "Eb", "E", "F", "F#", "G", "G#"];
+const OCTAVES = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const FREQUENCIES = [];
 const NOTES = [];
 
-for (let i = 1; i < NOTES_ON_GRAND_PIANO; i ++) {
-  const frequency = keyToFrequency(i);
+for (let i = 1; i <= 88; i ++) {
+  const note = teoria.note.fromKey(i);
 
-  NOTES.push(frequencyToNoteName(frequency));
-  FREQUENCIES.push(frequency);
+  NOTES.push(note.scientific());
+  FREQUENCIES.push(note.fq());
 }
 
-export { NOTES, FREQUENCIES };
+const { fromString, fromFrequency } = teoria.note;
 
-export const OCTAVES = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-export const NOTE_NAMES = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
-
-export function frequencyInRange(frequency) {
-  return frequency >= FREQUENCIES[0] && frequency <= FREQUENCIES[NOTES_ON_GRAND_PIANO - 1];
-}
-
-export function frequencyToNoteName(frequency) {
-  const key = frequencyToKey(frequency);
-  const octave = Math.floor(key / NOTES_PER_OCTAVE);
-  const noteIndex = key % NOTES_PER_OCTAVE;
-
-  return NOTE_NAMES[noteIndex] + octave;
-}
+export { NOTES, OCTAVES, NOTE_NAMES, FREQUENCIES, fromString, fromFrequency };
