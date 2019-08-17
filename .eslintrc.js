c5cc15db6c8e781de700fdaf558d7f324fd5e988
sustained <sustained.dissonance@gmail.com>
2019-08-17 01:47:47 +0100

    A little ESLint rule personalisation.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,22 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/no-unused-components": "off",
+    quotes: "off",
+    "space-before-function-paren": "off",
+    "space-unary-ops": [
+      1,
+      {
+        nonwords: true
+      }
+    ],
+    semi: "off"
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   }
-}
+};
